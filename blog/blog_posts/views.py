25fbaa11faef0822a88398d8fd4adb62fe4f0b96
Harsheet-saxena <harshsaxena1999@gmail.com>
2019-03-15 00:19:24 +0530

    article form added

--- blog/blog_posts/views.py
+++ blog/blog_posts/views.py
@@ -8,4 +8,4 @@ def posts_list(request, *args, **kwargs):
 
 def post_new(request, *args, **kwargs):
     form = ArticleForm()
-    return render(request,'' ,{'form': form})
+    return render(request,'blog_posts/post_article.html' ,{'form': form})
